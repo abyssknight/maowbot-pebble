3876b4d06a1bc0cf55b224689c6cfc8fbbf75527
Willa Riggins <abyss@abyssknight.com>
2014-12-09 22:18:27 -0500

    Added accelerometer based control.
    Added in accelerometer control and moved the menu based system to the up button. This seems to work with the current deadzone, but may need adjustment based on the user or use case. That said, it works! IP address is still hardcoded, and is currently set to use a mobile hotspot to save me time.

--- src/app.js
+++ src/app.js
@@ -4,15 +4,16 @@
 
 var UI = require('ui');
 var ajax = require('ajax');
+var Accel = require('ui/accel');
 
 var main = new UI.Card({
   title: 'MaowBot',
   icon: 'images/menu_icon.png',
   subtitle: 'Controller App',
-  body: 'Press select button.'
+  body: 'Press up button for menu, down for accelerometer.'
 });
 
-var URL = 'http://192.168.151.76:3000/api/';
+var URL = 'http://172.20.10.2:3000/api/';
 
 function sendCmd(cmd){  
   ajax(
@@ -35,7 +36,68 @@ function sendCmd(cmd){
 
 main.show();
 
-main.on('click', 'select', function(e) {
+var maowstatus = false;
+
+main.on('click', 'down', function(e) {
+  var card = new UI.Card({
+    title: "Accelerometer Control"
+  });
+  Accel.init();
+  card.on('accelData', function(e) {
+    // Average all the fields for each reading
+    var avg = {x: 0, y: 0, z: 0};    
+    for (var i = 0; i < e.accels.length; i++){
+      avg.x = avg.x + e.accels[i].x;
+      avg.y = avg.y + e.accels[i].y;
+      avg.z = avg.z + e.accels[i].z;      
+    }
+    avg.x = avg.x / e.accels.length;
+    avg.y = avg.y / e.accels.length;    
+    avg.z = avg.z / e.accels.length;    
+    
+    card.body(JSON.stringify(avg));
+    console.log(JSON.stringify(avg));
+    
+    if ((Math.abs(avg.x) < 500) && (Math.abs(avg.y) < 500)){
+      sendCmd('stop');
+      card.subtitle('stop');      
+    }
+    // Pos X is up (point hand up)
+    else if (avg.x > 600){
+      sendCmd('up');
+      card.subtitle('Up');
+    }     
+    // Neg X is down (point hand down)
+    else if (avg.x < -600){
+      sendCmd('down');    
+      card.subtitle('Down');      
+    }     
+    // Pos Y is right (rotate wrist)
+    if (avg.y > 600){
+      sendCmd('right');  
+      card.subtitle('Right');
+    }     
+    // Neg Y is left (rotate wrist)
+    else if (avg.y < -600){
+      sendCmd('left');    
+      card.subtitle('Left');
+    } 
+    
+  });
+  // Accel tap to stop
+  card.on('accelTap', function(e) {
+    if (maowstatus){
+      sendCmd('maowon');
+    } 
+    else {
+      sendCmd('maowoff');
+    }
+    maowstatus = !maowstatus;
+  });
+  card.show();
+});
+
+main.on('click', 'up', function(e) {
   var menu = new UI.Menu({
     sections: [{
       items: [{
